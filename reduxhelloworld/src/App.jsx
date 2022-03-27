13a4f2a327ba6d62104d66cfc1329be07c2becaf
parkgun6 <a01088807068@gmail.com>
2022-03-27 23:30:49 +0900

    feat: register 화면 route 추가

--- reduxhelloworld/src/App.jsx
+++ reduxhelloworld/src/App.jsx
@@ -1,11 +1,13 @@
+import RegisterTodo from 'page/RegisterTodo';
+import HelloWorld from 'page/HelloWorld';
 import React, { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HelloWorld from './page/HelloWorld';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HelloWorld />} />
+        <Route path='registertodo' element={<RegisterTodo />} />
       </Routes>
     </BrowserRouter>
   );
