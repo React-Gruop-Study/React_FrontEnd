82b0010d594ff14a072a23af443c80e732a49fa6
parkgun6 <a01088807068@gmail.com>
2022-03-27 23:31:58 +0900

    feat: 삭제로직 및 등록화면 이동 버튼 추가

--- reduxhelloworld/src/page/HelloWorld.jsx
+++ reduxhelloworld/src/page/HelloWorld.jsx
@@ -1,24 +1,38 @@
 import ResViewer from 'component/helloworld/ResViewer';
 import SnoInput from 'component/helloworld/SnoInput';
 import React, { useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { thunkGetHelloWorld } from 'store/helloworld/helloworldSlice';
+import {
+  thunkDeleteTodo,
+  thunkGetHelloWorld,
+} from 'store/helloworld/helloworldSlice';
 
 // page를 라우터로 분리하고, 컴포넌트안에서 페이지별 폴더구조를 나눈다.
 const HelloWorld = () => {
   const snoRef = useRef();
   const dispatch = useDispatch();
-
   const navigator = useNavigate();
+  const deleteSnoNum = useSelector((state) => state.todo.sno);
 
   // 각 api호출마다 하나의 함수로 분리한다.
   const saveSnoFn = () => {
     // dispatch는 fufilled시 then이 가능하다.
     dispatch(thunkGetHelloWorld(Number(snoRef.current.value))).then(() => {
-      navigator('/chicken');
+      // navigator('/chicken');
     });
     // catch는 rejected에서 관리한다.
+  };
+
+  const deleteSnoFn = () => {
+    const obj = {};
+    obj.sno = deleteSnoNum.toString();
+    console.log(JSON.stringify(obj));
+    dispatch(thunkDeleteTodo(JSON.stringify(obj)));
+  };
+
+  const linkToRegist = () => {
+    navigator('/registertodo');
   };
 
   // snoinput에서 클릭하여 조회하는 이벤트를 상위로올려서 함수를 파라미터로 전달한다.
@@ -26,6 +40,12 @@ const HelloWorld = () => {
     <div>
       <SnoInput ref={snoRef} onClick={saveSnoFn} />
       <ResViewer />
+      <button type='button' onClick={deleteSnoFn}>
+        현재 글 삭제하기
+      </button>
+      <button type='button' onClick={linkToRegist}>
+        새로운 텍스트 등록
+      </button>
     </div>
   );
 };
