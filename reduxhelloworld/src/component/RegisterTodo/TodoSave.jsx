690dfa9ece8449489e21128b832e8bd0215df8d4
parkgun6 <a01088807068@gmail.com>
2022-04-05 13:26:56 +0900

    fix: 이미지관련 로직 분리

--- reduxhelloworld/src/component/RegisterTodo/TodoSave.jsx
+++ reduxhelloworld/src/component/RegisterTodo/TodoSave.jsx
@@ -1,57 +1,13 @@
-import React, { forwardRef, useEffect, useState } from 'react';
+import React, { forwardRef } from 'react';
+import { useSelector } from 'react-redux';
 
 const TodoSave = forwardRef(({ onClick }, refFn) => {
-  const [src, setSrc] = useState();
-  const srcList = [];
-  const readMultipleImage = (input) => {
-    if (input.target.files) {
-      const fileArr = Array.from(input.target.files);
-      console.log(fileArr);
-      fileArr.forEach((file) => {
-        const reader = new FileReader();
-        console.log(file.name);
-        console.log(reader);
-        reader.onload = (e) => {
-          srcList.push(e.target.result);
-          setSrc(srcList);
-          // setSrc(e.target.result);
-        };
-        reader.readAsDataURL(file);
-      });
-    }
-  };
-  // eslint-disable-next-line consistent-return
-  // const imgList = () => {
-  //   if (src !== undefined) {
-  //     src.map((res) => {
-  //       console.log(res);
-  //       return <img src={res} width='550px' height='350px' />;
-  //     });
-  //   }
-  // };
-  // eslint-disable-next-line consistent-return
-  const imgList = () => {
-    console.log(src);
-    if (src !== undefined) {
-      return (
-        <div>
-          <img src={src[0]} width='550px' height='350px' />
-          <img src={src[1]} width='550px' height='350px' />
-          <img src={src[2]} width='550px' height='350px' />
-        </div>
-      );
-    }
-  };
   return (
     <div>
       <input type='text' ref={refFn} />
-      <div>
-        <input type='file' onChange={readMultipleImage} multiple />
-      </div>
       <button type='button' onClick={onClick}>
         저장하기
       </button>
-      <div className='imgDiv'>{imgList()}</div>
     </div>
   );
 });
