aaf13b0c9d3c740fdecc145f123822f89a3939a0
parkgun6 <a01088807068@gmail.com>
2022-04-11 18:07:58 +0900

    feat: 네비게이션바 생성

--- reduxhelloworld/src/common/BNB.jsx
+++ reduxhelloworld/src/common/BNB.jsx
@@ -1,10 +1,25 @@
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import React, { useState } from 'react';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { LinkToMain } from 'component/location/LinkTo';
+
+const useStyles = makeStyles({
+  root: {
+    bottom: 0,
+    '& .MuiBottomNavigationAction-root': {
+      '@media (max-width: 768px)': {
+        minWidth: 'auto',
+        padding: '6px 0',
+      },
+    },
+  },
+});
 
 const BNB = () => {
+  const classes = useStyles();
   const [value, setValue] = useState();
   return (
     <div>
@@ -14,6 +29,7 @@ const BNB = () => {
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
+        className={classes.root}
       >
         <BottomNavigationAction label='Recents' icon={<RestoreIcon />} />
         <BottomNavigationAction label='Favorites' icon={<FavoriteIcon />} />
---
+++ reduxhelloworld/src/common/GNB.jsx
@@ -0,0 +1,33 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import React from 'react';
+import { LinkToRegist, LinkToMain } from 'component/location/LinkTo';
+import { useNavigate } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
+
+const GNB = () => {
+  const navigator = useNavigate();
+
+  const linkToMainFn = () => {
+    navigator('/');
+  };
+  return (
+    <Box>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography
+            variant='h6'
+            component='div'
+            sx={{ flexGrow: 1 }}
+            onClick={linkToMainFn}
+          >
+            <Button color='inherit'>Journey</Button>
+          </Typography>
+          <LinkToRegist />
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default GNB;
