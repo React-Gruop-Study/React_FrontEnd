92ad171cddb2305b52106e114b8371f7cca39020
parkgun6 <a01088807068@gmail.com>
2022-04-11 18:11:19 +0900

    feat: modify 값 가져오는 로직 생성

--- reduxhelloworld/src/component/updatetodo/UpdateInput.jsx
+++ reduxhelloworld/src/component/updatetodo/UpdateInput.jsx
@@ -1,22 +1,44 @@
 import { LinkToMain } from 'component/location/LinkTo';
-import React, { forwardRef } from 'react';
-import { useSelector } from 'react-redux';
+import React, { forwardRef, useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { thunkGetTextWithImg } from 'store/helloworld/helloworldSlice';
 
 const UpdateViewer = forwardRef(({ onClick }, ref) => {
   const sno = useSelector((state) => state.todo.sno);
   const text = useSelector((state) => state.todo.text);
+  const modifytList = useSelector((state) => state.todo);
 
+  const [sno1, setSno1] = useState();
+  const [text1, setText1] = useState();
+  const [src, setSrc] = useState();
+
+  const dispatch = useDispatch();
+
+  const getTextWithImg = () => {
+    const snoNum = 7569;
+    dispatch(thunkGetTextWithImg(snoNum)).then((res) => {
+      setSno1(res.payload[0][0].sno);
+      setText1(res.payload[0][0].text);
+      setSrc(res.payload[0][1].path);
+    });
+  };
+
+  useEffect(() => {
+    getTextWithImg();
+    console.log(modifytList);
+  }, []);
   return (
     <div>
       <div>
-        sno : <input type='text' value={sno} readOnly />
+        sno : <input type='text' value={sno1} readOnly />
       </div>
       <div>
-        변경할 값 : <input type='text' defaultValue={text} ref={ref} />
+        변경할 값 : <input type='text' defaultValue={text1} ref={ref} />
       </div>
       <button type='button' onClick={onClick}>
         수정완료
       </button>
+      <img src={src} width='350' />
       <LinkToMain />
     </div>
   );
