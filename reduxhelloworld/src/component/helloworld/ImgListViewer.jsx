b5d7fa26e2ad4e4ce0ab32d5370c2faf96be4c14
parkgun6 <a01088807068@gmail.com>
2022-04-12 17:46:24 +0900

    feat: 삭제함수(deleteSnoFn) viewer로 위치 변경

--- reduxhelloworld/src/component/helloworld/ImgListViewer.jsx
+++ reduxhelloworld/src/component/helloworld/ImgListViewer.jsx
@@ -1,33 +1,48 @@
 import { makeStyles, Paper, Button, IconButton } from '@mui/material';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import BuildIcon from '@mui/icons-material/Build';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { thunkDeleteTodo } from 'store/helloworld/helloworldSlice';
 
 const ImgListViewer = ({ onClick }) => {
   const dtoList = useSelector((state) => state.todo.dtoList);
 
+  const dispatch = useDispatch();
+
+  const deleteSnoFn = async (sno) => {
+    await dispatch(thunkDeleteTodo(Number(sno))).then(() => {
+      window.location.replace('/');
+    });
+  };
   const returnDTOList = () => {
     return (
       dtoList && (
         <div>
-          {dtoList.map((el) => (
-            <Paper elevation={3} key={el.sno}>
-              <div key={el.sno}>
-                {el.sno}
+          {dtoList.map((res) => (
+            <Paper elevation={3} key={res.sno}>
+              <div key={res.sno}>
+                {res.sno}
                 <div style={{ float: 'right' }}>
-                  {el.regDate.substring(0, 10)}
+                  {res.regDate.substring(0, 10)}
                 </div>
-                <img src={el.imgDTOList[0].path} width='350' />
-                <div> {el.text}</div>
+                <img src={res.imgDTOList[0].path} width='350' />
+                <div> {res.text}</div>
                 <div>
-                  <Button type='button'>
+                  <Button type='button' onClick={() => onClick(res.sno)}>
                     수정하기
                     <BuildIcon />
                   </Button>
-                  <Button type='button' onClick={() => onClick(el.sno)}>
+                  <Button type='button' onClick={() => deleteSnoFn(res.sno)}>
                     <DeleteIcon />
                   </Button>
+                  {/* <Button type='button' onClick={() => modifySnoFn(res.sno)}>
+                    수정하기
+                    <BuildIcon />
+                  </Button>
+                  <Button type='button' onClick={() => deleteSnoFn(res.sno)}>
+                    <DeleteIcon />
+                  </Button> */}
                 </div>
               </div>
             </Paper>
